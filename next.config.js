ef1a4e71949d626e61c4b7bf676a5c92effd825d
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-10-20 00:16:46 +0300

    güncelleme

--- next.config.js
+++ next.config.js
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      //appDir: true,
-    },
-    images: {
-      domains: ['source.unsplash.com'],
-    },
-    
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.module.rules.push({
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[path][name].[ext]',
-              },
-            },
-          ],
-        });
-      }
-  
-      return config;
-    },
-    
-  }
-  
-  module.exports = nextConfig
-  
+const nextConfig = {}
+
+module.exports = nextConfig
